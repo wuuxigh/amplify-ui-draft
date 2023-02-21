7b49b526904ce02a4a5f3047c3bf7e81b6abc515
Caleb Pollman <cpollman@amazon.com>
2023-02-21 08:39:27 -0800

    remove Geo

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,6 +1,5 @@
 export * from './AccountSettings';
 export * from './Authenticator';
-export * from './Geo';
 export * from './Storage';
 export * from './InAppMessaging';
 export * from './ThemeProvider';
